d2ab395176a9f3507ddf7fc6db78bb96ebedf6a3
Webamz <chriszxn99@gmail.com>
2023-10-10 14:12:01 +0200

    typo

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -15,5 +15,5 @@ async def async_generator() -> Generator[float, None, None]:
         float: Random number between 0 and 10.
     """
     for _ in range(10):
-        await asyncio.sleep(1)i
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
